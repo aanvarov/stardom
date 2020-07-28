0b5b4ee693f80d1422b737af40916cd2ab68e3e1
Abror Anvarov <46844042+aanvarov@users.noreply.github.com>
2020-07-28 23:37:53 +0500

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -2,14 +2,22 @@ $(function () {
   $('.top__slider').slick({
     infinite: true,
     autoplay: true,
-    fade: true,
     autoplaySpeed: 2500,
-    arrows: false,
-    // customPaging: function (slider, i) {
-    //   var thumb = $(slider.$slides[i]).data();
-    //   return '<a>'+(i+1)+'</a>';
-    // },
-    dots: true
+    dots: true,
+    prevArrow:
+      '<button class="slider-btn slider-btn__left"><svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.24137 10.7583L0.54928 6.22845C0.261351 5.95047 0.261351 5.49979 0.54928 5.22181L5.24137 0.691944C5.5293 0.413969 5.99612 0.413969 6.28405 0.691944C6.57198 0.969919 6.57198 1.42061 6.28405 1.69858L2.17409 5.66645C2.17572 5.6858 2.17656 5.70537 2.17656 5.72513C2.17656 5.74489 2.17572 5.76446 2.17409 5.78381L6.28405 9.75168C6.57198 10.0297 6.57198 10.4803 6.28405 10.7583C5.99612 11.0363 5.5293 11.0363 5.24137 10.7583Z"/></svg></button>',
+    nextArrow:
+      '<button class="slider-btn slider-btn__right"><svg width="8" height="11" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.20883 10.7583L6.90092 6.22845C7.18884 5.95047 7.18884 5.49979 6.90092 5.22181L2.20883 0.691944C1.9209 0.413969 1.45407 0.413969 1.16614 0.691944C0.878213 0.969919 0.878213 1.42061 1.16614 1.69858L5.27611 5.66645C5.27447 5.6858 5.27364 5.70537 5.27364 5.72513C5.27364 5.74489 5.27447 5.76446 5.27611 5.78381L1.16614 9.75168C0.878213 10.0297 0.878213 10.4803 1.16614 10.7583C1.45407 11.0363 1.9209 11.0363 2.20883 10.7583Z"/></svg></button>',
+    responsive: [
+      {
+        breakpoint: 1030,
+        settings: {
+          prevArrow: false,
+          nextArrow: false
+        }
+      }
+    ]
+
   });
   $('.catalog__slider').slick({
     slidesToShow: 1,
@@ -74,8 +82,25 @@ $(function () {
     $($(this).data("target")).show().animate({ opacity: 1 }, 500);
   });
 
-  $('.navbar-toggler').click(function(){
+  $('.navbar-toggler').click(function () {
     $('.header__bottom__inner').toggle(300);
   });
+  
+  $('.about__info-btn').click(function () {
+    $('.showmore').toggle(400);
+  });
+
+  // $('.count').each(function () {
+  //   var $this = $(this);
+  //   jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+  //     duration: 1000,
+  //     easing: 'swing',
+  //     step: function () {
+  //       $this.text(Math.ceil(this.Counter));
+  //     }
+  //   })
+  // })
+
+  $('.count').rCounter();
 
 });
